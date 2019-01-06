642f6dc96e5eeb707b95e39b5210047d19d26a03
TJ Draper <tj@buzzingpixel.com>
2019-01-05 21:32:24 -0600

    Set up the action queue item model

--- src/models/ActionQueueBatchModel.php
+++ src/models/ActionQueueBatchModel.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace corbomite\queue\models;
 
 use DateTime;
-use corbomite\queue\models\ActionQueueItemModel;
 
 class ActionQueueBatchModel
 {
--- src/models/ActionQueueItemModel.php
+++ src/models/ActionQueueItemModel.php
@@ -3,6 +3,53 @@ declare(strict_types=1);
 
 namespace corbomite\queue\models;
 
+use DateTime;
+
 class ActionQueueItemModel
 {
+    private $guid = '';
+
+    public function guid(?string $guid = null): string
+    {
+        return $this->guid = $guid !== null ? $guid : $this->guid;
+    }
+
+    private $isFinished = false;
+
+    public function isFinished(?bool $isFinished = null): bool
+    {
+        return $this->isFinished = $isFinished !== null ?
+            $isFinished :
+            $this->isFinished;
+    }
+
+    private $finishedAt;
+
+    public function finishedAt(?DateTime $finishedAt = null): ?DateTime
+    {
+        return $this->finishedAt = $finishedAt !== null ?
+            $finishedAt :
+            $this->finishedAt;
+    }
+
+    private $class = '';
+
+    public function class(?string $class = null): string
+    {
+        return $this->class = $class !== null ? $class : $this->class;
+    }
+
+    private $method = '';
+
+    public function method(?string $method = null): string
+    {
+        return $this->method = $method !== null ? $method : $this->method;
+    }
+
+    private $context = [];
+
+    public function context(?array $context = null): array
+    {
+        return $this->context = $context !== null ? $context : $this->context;
+    }
 }
