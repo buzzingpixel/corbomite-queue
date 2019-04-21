a057de1086376c46296e2d8ec073cefff9111681
TJ Draper <tj@buzzingpixel.com>
2019-04-21 09:10:11 -0500

    Add is_running and assume_dead_after columns to batch table

--- src/data/ActionQueueBatch/ActionQueueBatchFields.php
+++ src/data/ActionQueueBatch/ActionQueueBatchFields.php
@@ -11,6 +11,8 @@ namespace corbomite\queue\data\ActionQueueBatch;
  * @property mixed $name varchar(255) NOT NULL
  * @property mixed $title varchar(255) NOT NULL
  * @property mixed $has_started tinyint(3,0) NOT NULL
+ * @property mixed $is_running tinyint(3,0) NOT NULL
+ * @property mixed $assume_dead_after datetime NOT NULL
  * @property mixed $is_finished tinyint(3,0) NOT NULL
  * @property mixed $finished_due_to_error tinyint(3,0) NOT NULL
  * @property mixed $percent_complete float(12) NOT NULL
@@ -19,6 +21,7 @@ namespace corbomite\queue\data\ActionQueueBatch;
  * @property mixed $finished_at datetime
  * @property mixed $finished_at_time_zone varchar(255)
  * @property mixed $context text(65535)
+ * @property mixed $assume_dead_after_time_zone varchar(255) NOT NULL
  * @property null|\corbomite\queue\data\ActionQueueItem\ActionQueueItemRecordSet $action_queue_items
  */
 trait ActionQueueBatchFields
--- src/data/ActionQueueBatch/ActionQueueBatchRow.php
+++ src/data/ActionQueueBatch/ActionQueueBatchRow.php
@@ -13,6 +13,8 @@ use Atlas\Table\Row;
  * @property mixed $name varchar(255) NOT NULL
  * @property mixed $title varchar(255) NOT NULL
  * @property mixed $has_started tinyint(3,0) NOT NULL
+ * @property mixed $is_running tinyint(3,0) NOT NULL
+ * @property mixed $assume_dead_after datetime NOT NULL
  * @property mixed $is_finished tinyint(3,0) NOT NULL
  * @property mixed $finished_due_to_error tinyint(3,0) NOT NULL
  * @property mixed $percent_complete float(12) NOT NULL
@@ -21,6 +23,7 @@ use Atlas\Table\Row;
  * @property mixed $finished_at datetime
  * @property mixed $finished_at_time_zone varchar(255)
  * @property mixed $context text(65535)
+ * @property mixed $assume_dead_after_time_zone varchar(255) NOT NULL
  */
 class ActionQueueBatchRow extends Row
 {
@@ -28,14 +31,17 @@ class ActionQueueBatchRow extends Row
         'guid' => null,
         'name' => null,
         'title' => null,
-        'has_started' => '0',
-        'is_finished' => '0',
-        'finished_due_to_error' => '0',
-        'percent_complete' => '0',
+        'has_started' => 0,
+        'is_running' => 0,
+        'assume_dead_after' => null,
+        'is_finished' => 0,
+        'finished_due_to_error' => 0,
+        'percent_complete' => 0.0,
         'added_at' => null,
         'added_at_time_zone' => null,
         'finished_at' => 'NULL',
         'finished_at_time_zone' => 'NULL',
         'context' => 'NULL',
+        'assume_dead_after_time_zone' => null,
     ];
 }
